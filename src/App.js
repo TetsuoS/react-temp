811e582f89c7eed1616d795fb7606ce3e03c70e4
MacBookPro <t-saito@MacBookPronoMacBook-Pro.local>
2020-04-07 11:44:17 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hello1">
+      <p>hello!</p>
+    </div>
+
+    <div className="hello2">
+      <p>hello2</p>
     </div>
   );
 }
