c3abb57054a0158d2adaa8fc6cf72d5ea405449e
keisan1231 <keisan1231@gmail.com>
2020-04-08 12:54:21 +0900

    異なる領域に異なるものを記述

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="hello1">
-      <p>hello!</p>
-    </div>
-
-    <div className="hello2">
-      <p>hello2</p>
+    <div>
+      <header className="App-header">ヘッダー(サイトのタイトルなど)</header>
+      <main className="main">
+        <div className="space1">
+          <h1 className="space1Text">領域1</h1>
+        </div>
+        <div className="space2">
+          <p className="space2Text">領域2</p>
+        </div>
+        <div className="space3">
+          <p className="space3Text">領域3</p>
+        </div>
+        <div className="side">
+          <div className="space4">
+            <h1 className="space1Text">領域4</h1>
+          </div>
+          <div className="space5">
+            <h1 className="space1Text">領域5</h1>
+          </div>
+          <div className="space6">
+            <h1 className="space1Text">領域6</h1>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
